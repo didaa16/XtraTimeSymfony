4e5757bb47412d9c05bf19ea60cc59c853fa3c48
didaa16 <ahmedhabib.mezni@esprit.tn>
2024-04-21 08:24:11 +0200

    Captcha

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Utilisateurs;
+use Karser\Recaptcha3Bundle\Form\Recaptcha3Type;
+use Karser\Recaptcha3Bundle\Validator\Constraints\Recaptcha3;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -135,6 +137,10 @@ class RegistrationFormType extends AbstractType
                         'max' => 4096,
                     ]),
                 ],
+            ])
+            ->add('captcha', Recaptcha3Type::class, [
+                'constraints' => new Recaptcha3(),
+                'action_name' => 'user',
             ]);
     }
 
